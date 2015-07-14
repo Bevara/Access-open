2a9efcbf4b65aecc627466b50319c5f6a312e94f
Jérôme Gorin <gorinje@gmail.com>
2015-07-14 20:29:40 +0200

    Start new accessor module

---
+++ modules/accessor_dec/accessor_comp.cpp
@@ -0,0 +1,226 @@
+/*
+ * accessor_comp.cpp
+ *
+ *  Created on: 13 juil. 2015
+ *      Author: gorinje
+ */
+
+#include "llvm/ADT/Triple.h"
+#include "llvm/ExecutionEngine/GenericValue.h"
+#include "llvm/ExecutionEngine/Interpreter.h"
+#include "llvm/ExecutionEngine/JITEventListener.h"
+#include "llvm/ExecutionEngine/MCJIT.h"
+#include "llvm/ExecutionEngine/ObjectCache.h"
+#include "llvm/ExecutionEngine/SectionMemoryManager.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IRReader/IRReader.h"
+#include "llvm/Support/MemoryBuffer.h"
+#include "llvm/Support/SourceMgr.h"
+#include "llvm/Support/TargetSelect.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/Program.h"
+#include "llvm/Support/Signals.h"
+
+#include "accessor_comp.h"
+
+using namespace llvm;
+using namespace std;
+
+struct Acc_Comp_s
+{
+	Module* Mod;
+	ExecutionEngine *EE;
+}Acc_Comp;
+
+//===----------------------------------------------------------------------===//
+// Object cache
+//
+// This object cache implementation writes cached objects to disk to the
+// directory specified by CacheDir, using a filename provided in the module
+// descriptor. The cache tries to load a saved object using that path if the
+// file exists. CacheDir defaults to "", in which case objects are cached
+// alongside their originating bitcodes.
+//
+class LLIObjectCache : public ObjectCache {
+public:
+	LLIObjectCache(const std::string& CacheDir) : CacheDir(CacheDir) {
+		// Add trailing '/' to cache dir if necessary.
+		if (!this->CacheDir.empty() &&
+			this->CacheDir[this->CacheDir.size() - 1] != '/')
+			this->CacheDir += '/';
+	}
+	virtual ~LLIObjectCache() {}
+
+	void notifyObjectCompiled(const Module *M, MemoryBufferRef Obj) override {
+		const std::string ModuleID = M->getModuleIdentifier();
+		std::string CacheName;
+		if (!getCacheFilename(ModuleID, CacheName))
+			return;
+		if (!CacheDir.empty()) { // Create user-defined cache dir.
+			SmallString<128> dir(CacheName);
+			sys::path::remove_filename(dir);
+			sys::fs::create_directories(Twine(dir));
+		}
+		std::error_code EC;
+		raw_fd_ostream outfile(CacheName, EC, sys::fs::F_None);
+		outfile.write(Obj.getBufferStart(), Obj.getBufferSize());
+		outfile.close();
+	}
+
+	std::unique_ptr<MemoryBuffer> getObject(const Module* M) override {
+		const std::string ModuleID = M->getModuleIdentifier();
+		std::string CacheName;
+		if (!getCacheFilename(ModuleID, CacheName))
+			return nullptr;
+		// Load the object from the cache filename
+		ErrorOr<std::unique_ptr<MemoryBuffer>> IRObjectBuffer =
+			MemoryBuffer::getFile(CacheName.c_str(), -1, false);
+		// If the file isn't there, that's OK.
+		if (!IRObjectBuffer)
+			return nullptr;
+		// MCJIT will want to write into this buffer, and we don't want that
+		// because the file has probably just been mmapped.  Instead we make
+		// a copy.  The filed-based buffer will be released when it goes
+		// out of scope.
+		return MemoryBuffer::getMemBufferCopy(IRObjectBuffer.get()->getBuffer());
+	}
+
+private:
+	std::string CacheDir;
+
+	bool getCacheFilename(const std::string &ModID, std::string &CacheName) {
+		std::string Prefix("file:");
+		size_t PrefixLength = Prefix.length();
+		if (ModID.substr(0, PrefixLength) != Prefix)
+			return false;
+		std::string CacheSubdir = ModID.substr(PrefixLength);
+#if defined(_WIN32)
+		// Transform "X:\foo" => "/X\foo" for convenience.
+		if (isalpha(CacheSubdir[0]) && CacheSubdir[1] == ':') {
+			CacheSubdir[1] = CacheSubdir[0];
+			CacheSubdir[0] = '/';
+		}
+#endif
+		CacheName = CacheDir + CacheSubdir+ ".o";
+		//size_t pos = CacheName.rfind('.');
+		//CacheName.replace(pos, CacheName.length() - pos, ".o");
+		return true;
+	}
+};
+
+
+static LLIObjectCache *CacheManager = nullptr;
+
+MDString* getFirstMDString(Module* mod, const char* attribute){
+	NamedMDNode* ND = mod->getNamedMetadata(attribute);
+	if (ND) {
+		MDNode* node = ND->getOperand(0);
+		if (node)
+			return cast<MDString>(node->getOperand(0));
+	}
+
+	return NULL;
+}
+
+int compile(Acc_Comp_s** comp, const char* llvm_code, unsigned int length, const char* cachedir){
+	bool enableCacheManager = false;
+	LLVMContext &Context = getGlobalContext();
+	InitializeNativeTarget();
+	InitializeNativeTargetAsmPrinter();
+	InitializeNativeTargetAsmParser();
+
+	// Create compiler
+	Acc_Comp_s* tmp_comp;
+	Module* Mod;
+
+	// Parse code
+	SMDiagnostic Err;
+	unique_ptr<MemoryBuffer> Buf = MemoryBuffer::getMemBuffer(StringRef(llvm_code, length));
+	unique_ptr<Module> Owner = parseIR(Buf.get()->getMemBufferRef(), Err, Context);
+
+	if (!Owner.get()) {
+		Err.print("Bevara module : ", errs());
+
+		//Parsing error
+		return 1;
+	}
+
+	Mod = Owner.get();
+
+
+	//Prepare cache name
+	string CacheName("file:");
+	MDString* libname = getFirstMDString(Mod, "LIBNAME");
+	if (libname && cachedir){
+		MDString* libversion = getFirstMDString(Mod, "LIBVERSION");
+		enableCacheManager = true;
+		CacheName.append(libname->getString());
+		if (libversion){
+			CacheName.append("-");
+			CacheName.append(libname->getString());
+		}
+		Mod->setModuleIdentifier(CacheName);
+	}
+
+	// Build engine
+	std::string ErrorMsg;
+	EngineBuilder builder(std::move(Owner));
+	builder.setErrorStr(&ErrorMsg);
+	builder.setOptLevel(CodeGenOpt::Aggressive);
+
+#ifdef WIN32
+	std::string TargetTriple = Mod->getTargetTriple();
+	TargetTriple.append("-elf");
+	// Temporary LLVM fix for windows
+	Mod->setTargetTriple(Triple::normalize(TargetTriple));
+#endif
+
+	// Enable MCJIT if desired.
+	RTDyldMemoryManager *RTDyldMM = nullptr;
+	RTDyldMM = new SectionMemoryManager();
+
+	// Deliberately construct a temp std::unique_ptr to pass in. Do not null out
+	// RTDyldMM: We still use it below, even though we don't own it.
+	builder.setMCJITMemoryManager(
+			std::unique_ptr<RTDyldMemoryManager>(RTDyldMM));
+
+	ExecutionEngine* EE = builder.create();
+	if (!EE) {
+		if (!ErrorMsg.empty())
+			errs() << "Accessor compilation error : error creating EE: " << ErrorMsg << "\n";
+		else
+			errs() << "Accessor compilation error : unknown error creating EE!\n";
+		return 1;
+	}
+
+	if (enableCacheManager) {
+		CacheManager = new LLIObjectCache(cachedir);
+		EE->setObjectCache(CacheManager);
+	}
+
+	// The following functions have no effect if their respective profiling
+	// support wasn't enabled in the build configuration.
+	EE->RegisterJITEventListener(
+		JITEventListener::createOProfileJITEventListener());
+	EE->RegisterJITEventListener(
+		JITEventListener::createIntelJITEventListener());
+
+	// Give MCJIT a chance to apply relocations and set page permissions.
+	EE->finalizeObject();
+	EE->runStaticConstructorsDestructors(false);
+
+
+
+	tmp_comp = new Acc_Comp_s();
+	tmp_comp->Mod = Mod;
+	tmp_comp->EE = EE;
+	*comp = tmp_comp;
+
+	return 0;
+}
+
+
+
+
---
+++ modules/accessor_dec/accessor_dec.c
@@ -0,0 +1,207 @@
+#include <gpac/modules/service.h>
+#include <gpac/modules/codec.h>
+#include <gpac/avparse.h>
+#include <gpac/constants.h>
+
+#include "accessor_comp.h"
+
+typedef struct
+{
+	u16 ES_ID;
+
+	Acc_Comp* comp;
+
+	GF_Err (*init)(char* data, unsigned int dataLength);
+	GF_Err (*entry)(char *inBuffer, u32 inBufferLength, char *outBuffer, u32 *outBufferLength, u8 PaddingBits, u32 mmlevel);
+	GF_Err (*shutdown)();
+	GF_Err (*set_caps)(GF_CodecCapability capability);
+	GF_Err (*caps)(GF_CodecCapability *capability);
+} Acc_dec;
+
+static GF_Err BV_AttachStream(GF_BaseDecoder *ifcg, GF_ESD *esd)
+{
+	GF_Err err = GF_OK;
+	char *llvm_code = NULL; 
+	u32 llvm_length;
+	char* data = NULL;
+	u32 dataLength = 0;
+	Acc_dec *ctx = NULL;
+	const char* cachedir = NULL;
+
+	printf("[Accessor decoder] Trying to use specific bevara decoder... \n");
+	ctx = (Acc_dec*) ifcg->privateStack;
+
+	/*not supported for a first version (and may be never be)*/
+	if (esd->dependsOnESID){
+		err = GF_NOT_SUPPORTED;
+		goto error;
+	}
+	llvm_length = esd->decoderConfig->rvc_config->dataLength;
+	if (!llvm_length){
+		err = GF_NOT_SUPPORTED;
+		goto error;
+	}
+	
+	printf("[Accessor decoder] Bevara decoder can be used! Starting decoder compilation... \n");
+
+	ctx->ES_ID = esd->ESID;
+	llvm_code = esd->decoderConfig->rvc_config->data;
+	llvm_code[llvm_length] = '\0'; //Todo : Copy data and then add the code end
+
+
+	cachedir = gf_modules_get_option((GF_BaseInterface *)ifcg, "General", "CacheDirectory");
+
+	// Init LLVM compilation
+	err = compile(&ctx->comp, llvm_code, llvm_length, cachedir);
+	if (err) goto error;
+	printf("[Accessor decoder] Compilation succeed! Start parsing useful metadata... \n");
+
+	//ctx->entry = getMDFn(ctx->comp, "ENTRY");
+	if (!ctx->entry){
+		err = GF_CORRUPTED_DATA;
+		goto error;
+	}
+	//ctx->init = getMDFn(ctx->comp, "INIT");
+	//ctx->shutdown = getMDFn(ctx->comp, "CLOSE");
+	//ctx->caps = getMDFn(ctx->comp, "CAPABILITIES");
+	//ctx->set_caps = getMDFn(ctx->comp, "SET_CAPABILITIES");
+
+	if (esd->decoderConfig->decoderSpecificInfo){
+		data = esd->decoderConfig->decoderSpecificInfo->data;
+		dataLength = esd->decoderConfig->decoderSpecificInfo->dataLength;
+	}
+
+
+	/* Initialize dec */
+	if (ctx->init){
+		printf("[Accessor decoder] Start initializing decoder..\n");
+		err = ctx->init(data, dataLength);
+
+		if (err) goto error;
+		printf("[Accessor decoder] Initializing succeed!\n");
+	}
+	else{
+		printf("[Accessor decoder] Decoder doesn't need initialization.\n");
+	}
+
+	//gf_free(llvm_code);
+	esd->decoderConfig->rvc_config->data = NULL;
+	esd->decoderConfig->rvc_config->dataLength = 0;
+
+	printf("[Accessor decoder] Accessor is ready to be used!\n");
+	return err;
+
+error:
+	fprintf(stderr,"[Accessor decoder] Unrecoverable error! Accessor can't be used for the following reason : %s", gf_error_to_string(err));
+	return err;
+}
+static GF_Err BV_DetachStream(GF_BaseDecoder *ifcg, u16 ES_ID)
+{
+	Acc_dec *ctx = (Acc_dec*) ifcg->privateStack;
+	if (ctx->shutdown)  ctx->shutdown();
+	return GF_OK;
+}
+
+static GF_Err BV_GetCapabilities(GF_BaseDecoder *ifcg, GF_CodecCapability *capability)
+{
+	GF_Err err;
+	Acc_dec *ctx = (Acc_dec*) ifcg->privateStack;
+	if (!ctx->caps) return GF_NOT_SUPPORTED;
+	err = ctx->caps(capability);
+	 return err;
+}
+
+static GF_Err BV_SetCapabilities(GF_BaseDecoder *ifcg, GF_CodecCapability capability)
+{
+	Acc_dec *ctx = (Acc_dec*) ifcg->privateStack;
+	if (!ctx->set_caps) return GF_NOT_SUPPORTED;
+	return ctx->set_caps(capability);
+}
+
+
+int bookmark = 0;
+
+static GF_Err BV_ProcessData(GF_MediaDecoder *ifcg, 
+		char *inBuffer, u32 inBufferLength,
+		u16 ES_ID,
+		char *outBuffer, u32 *outBufferLength,
+		u8 PaddingBits, u32 mmlevel)
+{
+	Acc_dec *ctx = (Acc_dec*) ifcg->privateStack;
+	return ctx->entry(inBuffer, inBufferLength, outBuffer, outBufferLength,0,0);
+}
+
+static u32 BV_CanHandleStream(GF_BaseDecoder *dec, u32 StreamType, GF_ESD *esd, u8 PL)
+{
+	if (esd && esd->decoderConfig && esd->decoderConfig->rvc_config) return GF_CODEC_SUPPORTED;
+	return GF_CODEC_NOT_SUPPORTED;
+}
+
+static const char *BV_GetCodecName(GF_BaseDecoder *dec)
+{
+	return "Bevara Decoder";
+}
+
+
+void Delete_Acc_dec(GF_BaseDecoder *ifcg)
+{
+	Acc_dec *ctx = (Acc_dec*) ifcg->privateStack;
+	gf_free(ctx);
+	gf_free(ifcg);
+}
+
+GF_BaseDecoder *New_BV_Decoder()
+{
+	GF_MediaDecoder *ifcd;
+	Acc_dec *dec;
+	
+	GF_SAFEALLOC(ifcd, GF_MediaDecoder);
+	GF_SAFEALLOC(dec, Acc_dec);
+	GF_REGISTER_MODULE_INTERFACE(ifcd, GF_MEDIA_DECODER_INTERFACE, "Bevara Decoder", "Bevara Technologies")
+
+	ifcd->privateStack = dec;
+
+	/*setup our own interface*/	
+	ifcd->AttachStream = BV_AttachStream;
+	ifcd->DetachStream = BV_DetachStream;
+	ifcd->GetCapabilities = BV_GetCapabilities;
+	ifcd->SetCapabilities = BV_SetCapabilities;
+	ifcd->GetName = BV_GetCodecName;
+	ifcd->CanHandleStream = BV_CanHandleStream;
+	ifcd->ProcessData = BV_ProcessData;
+	return (GF_BaseDecoder *) ifcd;
+}
+
+
+
+GPAC_MODULE_EXPORT
+const u32 *QueryInterfaces() 
+{
+	static u32 si [] = {
+		GF_MEDIA_DECODER_INTERFACE,
+		0
+	};
+	return si; 
+}
+
+GPAC_MODULE_EXPORT
+GF_BaseInterface *LoadInterface(u32 InterfaceType) 
+{
+	if (InterfaceType == GF_MEDIA_DECODER_INTERFACE) return (GF_BaseInterface *)New_BV_Decoder();
+	return NULL;
+}
+
+GPAC_MODULE_EXPORT
+void ShutdownInterface(GF_BaseInterface *ifce)
+{
+	switch (ifce->InterfaceType) {
+#ifndef GPAC_DISABLE_AV_PARSERS
+	case GF_MEDIA_DECODER_INTERFACE: 
+		Delete_Acc_dec((GF_BaseDecoder*)ifce);
+		break;
+#endif
+	}
+}
+
+
+GPAC_MODULE_STATIC_DECLARATION( Acc_dec )
