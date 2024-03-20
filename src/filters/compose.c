5fd47620137edac239be3ceafa67b8c919430c01
Jérôme Gorin <gorinje@gmail.com>
2024-03-20 16:19:01 +0100

    merge

--- src/filters/compose.c
+++ src/filters/compose.c
@@ -1223,7 +1223,7 @@ const GF_FilterRegister CompositorFilterRegister = {
 	.probe_url = compose_probe_url,
 };
 
-const GF_FilterRegister *compositor_register(GF_FilterSession *session)
+const GF_FilterRegister *dynCall_compose_filter_register(GF_FilterSession *session)
 {
 	u32 i=0;
 	u32 nb_args = sizeof(CompositorArgs) / sizeof(GF_FilterArgs) - 1;
@@ -1239,7 +1239,7 @@ const GF_FilterRegister *compositor_register(GF_FilterSession *session)
 	return &CompositorFilterRegister;
 }
 #else
-const GF_FilterRegister *compositor_register(GF_FilterSession *session)
+const GF_FilterRegister *dynCall_compose_filter_register(GF_FilterSession *session)
 {
 	return NULL;
 }
