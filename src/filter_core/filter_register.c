5f307f20eb7be9544e0b5ab6de57b8a59eac5899
Jérôme Gorin <gorinje@gmail.com>
2025-05-25 00:06:21 +0200

    Fix gf_fs_reg_all

--- src/filter_core/filter_register.c
+++ src/filter_core/filter_register.c
@@ -30,9 +30,6 @@
 #if defined(GPAC_CONFIG_EMSCRIPTEN)
 
 void gf_fs_reg_all(GF_FilterSession *fsess, GF_FilterSession *a_sess)
-{
-
-}
 
 GF_EXPORT
 void gf_fs_register_test_filters(GF_FilterSession *fsess)
